5d27999b863208efb3bda4fda7888cfc9f7fb5cd
Deya Card <thedeyacard@gmail.com>
2022-06-04 21:17:34 -0700

    add validations

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,11 +1,13 @@
 class Products < ApplicationRecord
   has_many :reviews, dependent: :destroy
-end
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin: true
+  validates_length_of :name, maximum: 100
+  before_save(:titleize_products)
 
-before_save(:titleize_products)
-
-private
-  def titleize_products
-    self.name = self.name.titleize
-  end
+  private
+    def titleize_products
+     self.name = self.name.titleize
+    end
 end
--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe Products do
   it { should have_many(:reviews) }
+
+  it { should validate_presence_of :name }
+
+  it { should validate_length_of(:name).is_at_most(100) }
+
+
 end
 
 describe Products do
