6d5a9ddb9b147e59b24b6d2a126029f610c90aeb
Deya Card <thedeyacard@gmail.com>
2022-06-04 22:24:29 -0700

    add scopes

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,9 +1,20 @@
 class Products < ApplicationRecord
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
+  scope :three_most_recent, -> { order(create_at: :desc).limit(3)}
+  scope :from_usa, -> { where(country_of_origin: "United States of America")}
+
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :cost, presence: true
+  validates :cost, numericality: true
   validates :country_of_origin: true
-  validates_length_of :name, maximum: 100
+  
   before_save(:titleize_products)
 
   private
--- app/models/reviews.rb
+++ app/models/reviews.rb
@@ -1,3 +1,8 @@
 class Reviews < ApplicationRecord
   belongs_to :products
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :content_body, length: { in: 50..250 }
+  validates :rating, presence: true
+  validates :rating, :numericality => { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
