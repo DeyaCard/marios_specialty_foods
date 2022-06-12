7b99ab5ba29bc3fa3e4203f5631c790880ec3fa8
Deya Card <thedeyacard@gmail.com>
2022-06-11 20:18:50 -0700

    correct typo in product_spec.rb file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Product do
   it { should have_many(:reviews) }
   it { should validate_presence_of :name }
-  it { should validate_presnce_of :cost }
+  it { should validate_presence_of :cost }
   it { should validate_presence_of :origin }
 
   it("will titleize the name of a product") do
