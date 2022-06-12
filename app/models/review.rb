8692e900b8524c5672b2bf1283b8195d7543a004
Deya Card <thedeyacard@gmail.com>
2022-06-11 20:11:48 -0700

    correct typo in review.rb file

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, :presence => true
   validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
-  validates :rating, presence => true
+  validates :rating, :presence => true
   validates :rating, :inclusion => { :in => [1,2,3,4,5]}
 end
